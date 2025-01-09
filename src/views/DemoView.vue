6afec9b92a5e461d9b5d5ccfe0bf91bda776f674
Gabriel <gabriel@swispace.com>
2025-01-09 07:07:02 +0100

    feat: demo page

--- src/views/DemoView.vue
+++ src/views/DemoView.vue
@@ -5,12 +5,28 @@ onMounted(() => {
   document.title = 'Optifit - Démo';
   window.scrollTo(0, 0);
 });
+
+onMounted(() => {
+  document.title = 'Optifit - Démo';
+  window.scrollTo(0, 0);
+
+  const script = document.createElement('script');
+  script.src = 'https://assets.calendly.com/assets/external/widget.js';
+  script.async = true;
+  document.body.appendChild(script);
+});
 </script>
 
 <template>
   <main>
     <div class="demo-section">
       <h1>Vous souhaitez <span>une démo ?</span></h1>
+      <h2>Organisons ça !</h2>
+      <div
+        class="calendly-inline-widget"
+        data-url="https://calendly.com/gabriel-optifit/demo-optifit?hide_gdpr_banner=1"
+        style="min-width: 320px; height: 700px"
+      ></div>
     </div>
   </main>
 </template>
@@ -27,6 +43,13 @@ onMounted(() => {
     span {
       color: var(--p-primary-color);
     }
+  }
+
+  h2 {
+    font-size: 1.5rem;
+    text-align: center;
+    font-weight: 500;
+    margin: 20px 0;
   }
 }
 </style>
