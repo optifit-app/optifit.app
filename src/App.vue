4b0f64c70a22458e5ce09c5f02874d33c765523e
Gabriel <gabriel@swispace.com>
2025-03-24 06:41:50 +0100

    chore: setup posthog

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <script setup lang="ts">
 import FooterComponent from './components/FooterComponent.vue';
 import NavigationBar from './components/NavigationBar.vue';
+import posthog from 'posthog-js';
+
+posthog.init('phc_OAobrL6ThZuC3jK3h8OxcLwOIa0sUk5IkokFEcTZYZX', {
+  api_host: 'https://eu.i.posthog.com',
+  person_profiles: 'always',
+});
 </script>
 
 <template>
