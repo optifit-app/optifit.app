6b2098ceaa768045e8e31dd80a25c045aea48f94
Gabriel <gabriel@swispace.com>
2025-05-06 06:38:39 +0200

    chore: reset vite project

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,7 @@
-import { fileURLToPath, URL } from 'node:url';
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import vueJsx from '@vitejs/plugin-vue-jsx';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
-});
+  plugins: [react()],
+})
