ff9eb04e77edf5a8bd5b43b7c39867e74bea5ed3
Gabriel <gabriel@swispace.com>
2025-08-08 23:44:56 +0200

    fix: responsiveness

--- src/pages/changelog.tsx
+++ src/pages/changelog.tsx
@@ -2,11 +2,16 @@ import { ChangelogEntries } from '@/components/ui/changelog';
 import icon from '@/assets/images/icon.png';
 import { type FC, useEffect, useState } from 'react';
 import { parseNotionChangelog } from '@/lib/notion.ts';
+import { ArrowLeft } from 'lucide-react';
+import { Button } from '@/components/ui/button.tsx';
+import { useNavigate } from 'react-router-dom';
 
 type ChangelogProps = object;
 
 const Changelog: FC<ChangelogProps> = () => {
   const [raw, setRaw] = useState<any[]>([]);
+
+  const navigate = useNavigate();
 
   useEffect(() => {
     const initChangelog = async (): Promise<void> => {
@@ -21,20 +26,21 @@ const Changelog: FC<ChangelogProps> = () => {
   }, []);
 
   return (
-    <div className="px-3">
-      <div className="flex flex-col items-center gap-3 mt-10 w-full">
-        <div className="flex items-center flex-wrap w-full justify-center gap-5">
+    <div className="px-10 flex flex-col gap-5 w-full items-center">
+      <div className="flex flex-col gap-3 mt-10 mx-auto">
+        <Button
+          variant="link"
+          className="mb-5"
+          onClick={() => navigate('/')}
+        >
+          <ArrowLeft />
+          Retour
+        </Button>
+        <div className="flex flex-col items-center gap-4">
           <img src={icon} alt="icon" className="h-18 w-18" />
-          <div>
-            <h1 className="text-3xl font-semibold text-pretty">Changelog</h1>
-            <h2 className="text-sm text-pretty">
-              {new Date().toLocaleString('fr-CH', {
-                dateStyle: 'full',
-              })}
-            </h2>
-          </div>
+          <h1 className="text-3xl font-semibold text-pretty">Changelog</h1>
         </div>
-        <p className="text-lg text-pretty text-muted-foreground">
+        <p className="text-lg text-pretty text-muted-foreground text-center">
           Suivez les dernières mises à jour de Optifit
         </p>
       </div>
