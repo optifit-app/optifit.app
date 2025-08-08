fe2d0333f7a6abe8d77a1ea714adf9440cf63a2a
Gabriel <gabriel@swispace.com>
2025-08-08 18:16:29 +0200

    feat: display changelog

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,10 @@ import { Privacy } from '@/pages/privacy.tsx';
 import { Terms } from '@/pages/terms.tsx';
 import { Button } from '@/components/ui/button.tsx';
 import { useTheme } from '@/components/providers/theme-provider.tsx';
-import { Moon, Sun } from 'lucide-react';
+import { Instagram, Linkedin, Moon, Sun } from 'lucide-react';
+import Changelog from '@/pages/changelog.tsx';
+import icon from '@/assets/images/icon.png';
+import { Footer } from '@/components/ui/footer.tsx';
 
 type AppProps = object;
 
@@ -19,9 +22,70 @@ const App: FC<AppProps> = () => {
     <div className="relative">
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/changelog" element={<Changelog />} />
         <Route path="/privacy-policy" element={<Privacy />} />
         <Route path="/terms-and-conditions" element={<Terms />} />
       </Routes>
+      <Footer
+        logo={{
+          url: 'https://www.optifit.app',
+          src: icon,
+          alt: 'Optifit Logo',
+          title: 'Optifit',
+        }}
+        description="Optifit est une application de gestion de tournois sportifs, développée en Suisse. Notre objectif est de simplifier l'organisation de vos événements sportifs avec une interface intuitive et des fonctionnalités puissantes."
+        copyright={`© ${new Date().getFullYear()} Optifit. Tous droits réservés.`}
+        sections={[
+          {
+            title: 'Liens utiles',
+            links: [
+              { name: 'Changelog', href: '/changelog' },
+              { name: 'Questions fréquentes', href: '/#faq' },
+              { name: 'Tarifs', href: '/#pricing' },
+              { name: 'Je veux une démo', href: 'https://demo.optifit.app' },
+            ],
+          },
+          {
+            title: 'Support',
+            links: [
+              {
+                name: "Demander de l'aide",
+                href: 'mailto:support@optifit.app',
+              },
+              { name: 'Politique de confidentialité', href: '/privacy-policy' },
+              {
+                name: 'Conditions d’utilisation',
+                href: '/terms-and-conditions',
+              },
+            ],
+          },
+          {
+            title: 'Réseaux sociaux',
+            links: [
+              {
+                name: 'Instagram',
+                href: 'https://www.instagram.com/optifit.app/',
+              },
+              {
+                name: 'LinkedIn',
+                href: 'https://www.linkedin.com/company/optifit-software',
+              },
+            ],
+          },
+        ]}
+        socialLinks={[
+          {
+            icon: <Instagram />,
+            href: 'https://www.instagram.com/optifit.app/',
+            label: 'Instagram',
+          },
+          {
+            icon: <Linkedin />,
+            href: 'https://www.linkedin.com/company/optifit-software',
+            label: 'LinkedIn',
+          },
+        ]}
+      />
       <Button
         variant="ghost"
         size="icon"
